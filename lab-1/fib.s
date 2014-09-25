c1d47ce5f84151833bc8d746c5bda6b0a529244d
atlantis0914 <atlantis0914@hotmail.com>
2014-09-25 10:56:36 +0000

    Update fib.s

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -15,13 +15,13 @@ fibonacci:
 
 	@ R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
-	subs r4, r0, #0		;if r4 == 0, Z = 1
-	cmp r0, #0		;this line is no longer needed because Z has alread set
-	ble.L3			;jump to L3 if R0 is less or equal to 0
+	subs r4, r0, #0		@if r4 == 0, Z = 1
+	cmp r0, #0		@this line is no longer needed because Z has alread set
+	ble.L3			@jump to L3 if R0 is less or equal to 0
 	@ Compare R4 wtih 1
 	@ If R4 == 1 goto .L4 (which returns 1)
-	cmp r4, #1		;if r4 == 1, Z = 1
-	beq .L4			;jump to L4 if R0 is less or equal to 1
+	cmp r4, #1		@if r4 == 1, Z = 1
+	beq .L4			@jump to L4 if R0 is less or equal to 1
 	@ R0 = R4 - 1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
 	sub r0, r4, #1
